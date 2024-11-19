77b3841c3ebb25da1dffa2e753eacba7ae6434e9
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-11-19 16:11:44 +0900

    [수정] 프로젝트 이름 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'Kaizen Dev Hearth',
+  title: 'Kaizen Dev Historia',
   description: "DongHyeon Kim's portfoilo",
 };
 
